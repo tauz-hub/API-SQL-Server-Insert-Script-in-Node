f64a9693eaecf63c5e15a9939cfcbd527d54ae8c
tauz-hub <tauasanto101@gmail.com>
2022-03-21 22:26:25 -0300

    finishing all the functions of creating entities

--- WGCarSolutionInsertInto.js
+++ WGCarSolutionInsertInto.js
@@ -17,10 +17,9 @@ function criaEndereco() {
   iEndereco++;
   return endereco
 }
-while (iEndereco < 10) {
+while (iEndereco < 100) {
   enderecos.push(criaEndereco())
 }
-
 
 let iSetor = 0;
 const nomeSetores = ["borracharia", "eletrica", "vidro", "pintura", "mecânica"];
@@ -89,6 +88,7 @@ function criaTelefone() {
   }
   numero = numero.join('')
 
+
   const telefone = {
     id_telefone: iTelefone,
     id_pessoa: Math.floor(Math.random() * 2) ? clientes[Math.floor(Math.random() * clientes.length)].cpf : funcionarios[Math.floor(Math.random() * funcionarios.length)].cpf,
@@ -101,10 +101,27 @@ while (telefones.length < 10) {
   telefones.push(criaTelefone())
 }
 
-
 Date.prototype.subDays = function (days) {
   const date = new Date(this.valueOf());
   date.setDate(date.getDate() - days);
+  return date;
+}
+
+Date.prototype.addHours = function (hours) {
+  const date = new Date(this.valueOf());
+  date.setHours(date.getHours() + hours);
+  return date;
+}
+
+Date.prototype.addMinutes = function (minutes) {
+  const date = new Date(this.valueOf());
+  date.setMinutes(date.getMinutes() + minutes);
+  return date;
+}
+
+Date.prototype.addSeconds = function (seconds) {
+  const date = new Date(this.valueOf());
+  date.setSeconds(date.getSeconds() + seconds);
   return date;
 }
 
@@ -141,7 +158,159 @@ function criaVeiculo() {
   return veiculo
 }
 
-while (veiculos.length < 10) {
+while (veiculos.length < 100) {
   veiculos.push(criaVeiculo())
 }
 
+
+let funcionarioSetores = []
+function criaFuncionarioDosSetores() {
+  const funcionarioSetor = {
+    id_funcionario: Math.floor(Math.random() * funcionarios.length),
+    id_setor: Math.floor(Math.random() * setores.length)
+  }
+  return funcionarioSetor;
+}
+while (funcionarioSetores.length < 10) {
+  funcionarioSetores.push(criaFuncionarioDosSetores())
+}
+
+let iServicos = 0;
+const servicos = []
+function criaServicos() {
+
+  const dicTempoExecucao = {
+    borracharia: () => `${Math.floor(Math.random() * (4 - 2)) + 2}:00h`,
+    eletrica: () => `${Math.floor(Math.random() * (7 - 4)) + 2}:00h`,
+    vidro: () => `${Math.floor(Math.random() * (15 - 6)) + 2}:00h`,
+    pintura: () => `${Math.floor(Math.random() * (24 - 14)) + 2}:00h`,
+    mecânica: () => {
+      return `${Math.floor(Math.random() * (7 - 2)) + 2}:00h`
+    }
+  }
+
+  const rndSetor = Math.floor(Math.random() * setores.length)
+
+  const nomeSetor = setores.find(setor => setor.id_setor == rndSetor).nome
+  const carroId = Math.floor(Math.random() * veiculos.length)
+
+  const dataDoCarro = veiculos[carroId].data_fabricacao.split('/')
+  const dataConvertidaCarro = new Date(`${dataDoCarro[2]}-${dataDoCarro[1]}-${dataDoCarro[0]}`)
+
+  const timeDiff = Math.abs(new Date().getTime() - dataConvertidaCarro.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  const data_registro = dataConvertidaCarro.addDays(Math.floor(Math.random() * diffDays)).addHours(Math.floor(Math.random() * 24)).addMinutes(Math.floor(Math.random() * 60)).addSeconds(Math.floor(Math.random() * 60))
+
+  const servico = {
+    id_servico: iServicos,
+    tempo_execucao: dicTempoExecucao[nomeSetor](),
+    data_registro: data_registro,
+    id_setor: rndSetor,
+    id_veiculo: carroId
+
+  }
+
+  iServicos++;
+  return servico
+}
+
+while (servicos.length < 10) {
+  servicos.push(criaServicos())
+}
+
+let iAgendamento = 0;
+const agendamentos = []
+function criaAgendamento() {
+
+  let saida = false;
+  let agendamento = {}
+  while (!saida) {
+
+    const servicoId = Math.floor(Math.random() * servicos.length)
+
+    let servicoRepetido = false
+
+    agendamentos.forEach((agendamento) => {
+      if (agendamento?.id_servico == servicoId) servicoRepetido = true
+    })
+
+
+    if (!servicoRepetido) {
+
+      const servico_data_registro = servicos[servicoId].data_registro
+
+      const dataAgendamento = servico_data_registro.addDays(Math.floor(Math.random() * 365))
+
+      agendamento = {
+        id_agendamento: iAgendamento,
+        data_agendamento: dataAgendamento.toLocaleString().substr(0, 10),
+        id_servico: servicoId,
+      }
+
+      iAgendamento++;
+      saida = true
+
+    }
+  }
+  return agendamento
+}
+
+while (agendamentos.length < 5 && agendamentos.length < servicos.length) {
+  agendamentos.push(criaAgendamento())
+}
+
+const dicTipoServico = {
+  borracharia: () => {
+    const listBorrachariaServicos = ["troca de pneu", "restauração de pneus", "troca da câmara de ar"]
+    return listBorrachariaServicos[`${Math.floor(Math.random() * listBorrachariaServicos.length)}`]
+  },
+  eletrica: () => {
+    const listEletricaServicos = ["troca de som", "troca de faróis", "instalação de painel eletrônico"]
+    return listEletricaServicos[`${Math.floor(Math.random() * listEletricaServicos.length)}`]
+  },
+  vidro: () => {
+    const listVidroServicos = ["troca de janelas dianteiras", "troca de janelas traseiras", "troca do para-brisa "]
+    return listVidroServicos[`${Math.floor(Math.random() * listVidroServicos.length)}`]
+  },
+  pintura: () => {
+    const listPinturaServicos = ["Pintura da lataria do carro", "pintura de pneus", "pintura de bancos"]
+    return listPinturaServicos[`${Math.floor(Math.random() * listPinturaServicos.length)}`]
+  },
+  mecânica: () => {
+    const listMecânicaServicos = ["troca de motor", "troca de óleo", "troca de eixo", "troca de filtros"]
+    return listMecânicaServicos[`${Math.floor(Math.random() * listMecânicaServicos.length)}`]
+  }
+}
+
+let iTipoServico = 0
+const tipoServicos = []
+function criaTipoServico() {
+  let saida = false;
+  let tipoServico
+  while (!saida) {
+
+    let servicoRepetido = false
+    const setor = setores[Math.floor(Math.random() * setores.length)]
+    const tipoDeServicoDoSetor = dicTipoServico[setor.nome]()
+
+    tipoServicos.forEach((tiposetor) => {
+      if (tiposetor?.tipo_servico == tipoDeServicoDoSetor) servicoRepetido = true
+    })
+
+
+    if (!servicoRepetido) {
+      tipoServico = {
+        id_tipo_servico: iTipoServico,
+        tipo_servico: tipoDeServicoDoSetor,
+        id_setor: setor.id_setor
+      }
+      saida = true
+    }
+  }
+  iTipoServico++
+  return tipoServico
+}
+
+while (tipoServicos.length < 16) {
+  tipoServicos.push(criaTipoServico())
+}
